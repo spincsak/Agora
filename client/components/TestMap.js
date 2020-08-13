b614774bcbf2b2a8b345f6648bccda3d94c52e6e
Simone Pincsak <simone.pincsak@gmail.com>
2020-08-13 18:13:06 -0500

    setting up leaflet basics

---
+++ client/components/TestMap.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import Leaflet from 'leaflet'
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+
+export class TestMap extends Component {
+  render() {
+    const position = [41.976015, -87.671499]
+    const zoomLevel = 14
+
+    return <h1>Hello World</h1>
+  }
+}
