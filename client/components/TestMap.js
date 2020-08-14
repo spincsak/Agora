2b33d2198e775a382beefcc6aea73de703a52ef9
Simone Pincsak <simone.pincsak@gmail.com>
2020-08-14 10:12:34 -0500

    a working map appears!

--- client/components/TestMap.js
+++ client/components/TestMap.js
@@ -7,6 +7,19 @@ export class TestMap extends Component {
     const position = [41.976015, -87.671499]
     const zoomLevel = 14
 
-    return <h1>Hello World</h1>
+    return (
+      <div>
+        <h1>Here is a Map!</h1>
+        <Map center={position} zoom={zoomLevel} id="mapid">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[41.976543, -87.671234]}>
+            <Popup>Hello World</Popup>
+          </Marker>
+        </Map>
+      </div>
+    )
   }
 }
